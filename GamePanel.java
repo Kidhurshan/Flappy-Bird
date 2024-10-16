eb8df79dad07fed71c98377c4c355b5615629fbb
Kidhurshan <d.kidhu@gmail.com>
2024-10-16 13:24:16 +0530

    creating the panel file

---
+++ GamePanel.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GamePanel extends JPanel implements KeyListener {
+    private int panelHorizontal = 640;
+    private int panelVertical = 360;
+    private Image birdImage;
+    private Image bottomPipe;
+    private Image topPipe;
+    private Image background;
+
+    public GamePanel(){
+        setPreferredSize(new Dimension(panelHorizontal,panelVertical));
+        setFocusable(true);
+        addKeyListener(this);
+
+        //set up the images
+        try{
+            birdImage = new ImageIcon(getClass().getResource("flappybird.png")).getImage();
+            background = new ImageIcon(getClass().getResource("flappybirdbg.png")).getImage();
+            bottomPipe = new ImageIcon(getClass().getResource("bottompipe.png")).getImage();
+            topPipe = new ImageIcon(getClass().getResource("toppipe.png")).getImage();
+        }catch(NullPointerException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void draw(Graphics g) {
+
+    }
+
+
+    @Override
+    public void paint(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
